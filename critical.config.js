a85b4ea53088205be5a7e51fa7d89818d5636f2f
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2024-07-14 13:19:18 +0300

    Update critical.config.js

--- critical.config.js
+++ critical.config.js
@@ -1,48 +1,29 @@
 import { generate } from 'critical';
 
-
 console.log('Current directory:', process.cwd());
 
-// generate({
-// 	inline: true,
-// 	base: 'build/',
-//   src: 'index.html',
-//   target: {
-//     html: 'index-critical.html',
-//     css: 'critical.css',
-//   },
-// 	width: 1200,  
-//   height: 900,
-// 	extract: true,
-// })
-// 	.then(({ html, css }) => {
-// 		console.log('Critical CSS generated successfully');
-// 	})
-// 	.catch(error => {
-// 		console.log(error);
-// 	});
-
-	generate({
-		inline: true,
-		base: 'build/',
-		src: 'index.html',
-		css: ['build/static/css/main.4bd2692d.css'],
-		width: 1300,
-		height: 900,
-		target: {
-			css: 'critical.css',
-			html: 'index-critical.html',
-			uncritical: 'uncritical.css',
-		},
-		extract: true,
-		ignore: {
-			atrule: ['@font-face'],
-			rule: [/some-regexp/],
-			decl: (node, value) => /big-image\.png/.test(value),
-		},
-	}).then(({ html, css }) => {
-				console.log('Critical CSS generated successfully');
-			})
-			.catch(error => {
-				console.log(error);
-			});
+generate({
+	inline: true,
+	base: 'build/',
+	src: 'index.html',
+	css: ['build/static/css/main.4bd2692d.css'],
+	width: 1300,
+	height: 900,
+	target: {
+		css: 'critical.css',
+		html: 'index-critical.html',
+		uncritical: 'uncritical.css',
+	},
+	extract: true,
+	ignore: {
+		atrule: ['@font-face'],
+		rule: [/some-regexp/],
+		decl: (node, value) => /big-image\.png/.test(value),
+	},
+})
+	.then(({ html, css }) => {
+		console.log('Critical CSS generated successfully');
+	})
+	.catch(error => {
+		console.log(error);
+	});
