0931ede7d98162488b90255392c0c0c09d1afa43
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2024-07-14 12:54:33 +0300

    + CompressionWebpackPlugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,19 @@
 const { override, addWebpackPlugin } = require('customize-cra');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = override(
-  addWebpackPlugin(
-    new CompressionWebpackPlugin({
-      test: /\.(js|css|html|svg)$/,
-      filename: '[path][base].gz',
-    })
-  )
+  config => {
+    if (config.mode === 'production') {
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          test: /\.(js|css|html|svg)$/,
+          filename: '[path][base].gz',
+        })
+      );
+    }
+    return config;
+  }
 );
