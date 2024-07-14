ab5a819166a4f2a8c78cc48678237c797cb14f62
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2024-07-14 14:47:51 +0300

    add small bg for hero

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,22 @@
-const { override, addWebpackPlugin } = require('customize-cra');
+const { override} = require('customize-cra');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const ImageminPlugin = require('imagemin-webpack-plugin').default;
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = override(
-  config => {
+  (config) => {
     if (config.mode === 'production') {
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true,
+            },
+          },
+        }),
+      ];
+
       config.plugins.push(
         new CompressionWebpackPlugin({
           test: /\.(js|css|html|svg)$/,
