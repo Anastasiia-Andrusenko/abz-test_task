c3a66cda98c74d5c718a335ee5a9ad992d81b540
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2024-07-15 12:57:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
 import './styles/main.scss';
 
-// Реєстрація Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('SW registered: ', registration);
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError);
-    });
-  });
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
