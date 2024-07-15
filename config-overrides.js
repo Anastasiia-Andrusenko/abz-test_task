c54f52508a5c638160dac67a0a07e2aaa9f39ac9
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2024-07-15 11:45:50 +0300

    + Service Worker

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,8 @@
-const { override} = require('customize-cra');
+const { override } = require('customize-cra');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-// const ImageminPlugin = require('imagemin-webpack-plugin').default;
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
+
 
 module.exports = override(
   (config) => {
@@ -21,6 +21,32 @@ module.exports = override(
         new CompressionWebpackPlugin({
           test: /\.(js|css|html|svg)$/,
           filename: '[path][base].gz',
+        }), new GenerateSW({
+          clientsClaim: true,
+          skipWaiting: true,
+          exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
+          navigateFallback: '/index.html',
+          runtimeCaching: [
+            {
+              urlPattern: new RegExp('^https://fonts\\.googleapis\\.com/'),
+              handler: 'StaleWhileRevalidate',
+            },
+            {
+              urlPattern: new RegExp('^https://fonts\\.gstatic\\.com/'),
+              handler: 'StaleWhileRevalidate',
+            },
+            {
+              urlPattern: /\.(?:png|jpg|jpeg|svg|webp|gif)$/,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'images',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+                },
+              },
+            },
+          ],
         })
       );
     }
